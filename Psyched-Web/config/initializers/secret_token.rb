1b8e63acb2f0a478cbc99cd39f6a2ecc6bdf6875
HengHong <henghong.lee@gmail.com>
2012-08-03 10:50:45 +0800

    added web basic

---
+++ Psyched-Web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsychedWeb::Application.config.secret_token = 'ee5aa641a1939ee868708ad3976d1275bff550c55a1f5388d4b53c084139ca7a050ade2dd67a9d0adf8fd00034dce841abe71e99f0ae76006dae5d9fa4b575c1'
